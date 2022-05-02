7e288177794a229a60d6339fc5f13a89576cca3e
bandeiraos <pedrocargninbandeira@gmail.com>
2022-05-02 17:22:23 -0300

    init: full application

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './../public/styles/main.css';
+import './../public/styles/media-query.css';
+import { Elm } from './Main.elm';
+
+Elm.Main.init({
+  node: document.getElementById('root')
+});
