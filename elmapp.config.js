d3d7c6a9c83498fe9935d05dbfcf1c42ce286885
bandeiraos <pedrocargninbandeira@gmail.com>
2022-05-02 17:52:11 -0300

    build folder

--- elmapp.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    homepage: "./" //required to normalise path
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './../public/styles/main.css';
-import './../public/styles/media-query.css';
-import { Elm } from './Main.elm';
-
-Elm.Main.init({
-  node: document.getElementById('root')
-});
